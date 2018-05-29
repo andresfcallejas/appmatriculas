611a7286ca9ea4a8f15db0e200a649b8cf643da3
Usuario <Usuario@10.126.13.98>
2018-05-29 18:01:38 -0500

    version final

---
+++ AppRegistroMatricula/src/main/java/com/co/poli/AppRegistroMatricula/AppRegistroMatriculaApplication.java
@@ -0,0 +1,12 @@
+package com.co.poli.AppRegistroMatricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRegistroMatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRegistroMatriculaApplication.class, args);
+	}
+}
---
+++ AppRegistroMatricula/src/main/java/com/co/poli/AppRegistroMatricula/model/Materia.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.co.poli.AppRegistroMatricula.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Materia {
+    private String idMateria;
+    private String nombre;
+    private double numCreditos;
+    private double cupoMin;
+    private double cupoMax;
+
+    public Materia() {
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.idMateria);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Materia other = (Materia) obj;
+        if (!Objects.equals(this.idMateria, other.idMateria)) {
+            return false;
+        }
+        return true;
+    }
+    
+
+    public String getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(String idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getNumCreditos() {
+        return numCreditos;
+    }
+
+    public void setNumCreditos(double numCreditos) {
+        this.numCreditos = numCreditos;
+    }
+
+    public double getCupoMin() {
+        return cupoMin;
+    }
+
+    public void setCupoMin(double cupoMin) {
+        this.cupoMin = cupoMin;
+    }
+
+    public double getCupoMax() {
+        return cupoMax;
+    }
+
+    public void setCupoMax(double cupoMax) {
+        this.cupoMax = cupoMax;
+    }
+    
+}
